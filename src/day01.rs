a4c984122e6d8f6001eb36f832eb9dfdeedfe824
Bart Slinger <bartslinger@gmail.com>
2021-12-01 21:26:45 +0100

    no need for pub functions

--- src/day01.rs
+++ src/day01.rs
@@ -17,14 +17,14 @@ mod tests {
     }
 }
 
-pub fn count_increments(input: &Vec<u32>) -> u32 {
+fn count_increments(input: &Vec<u32>) -> u32 {
     input.as_slice()
         .windows(2)
         .map(|w| (w[1] > w[0]) as u32)
         .sum()
 }
 
-pub fn count_sliding_window_increments(input: &Vec<u32>) -> u32 {
+fn count_sliding_window_increments(input: &Vec<u32>) -> u32 {
     let window_sums: Vec<u32> = input.as_slice()
         .windows(3)
         .map(|w| w.iter().sum())
