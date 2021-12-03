80a0c860a5483d16d6bce60ce0bfa1396ed2ccda
Bart Slinger <bartslinger@gmail.com>
2021-12-03 18:24:03 +0100

    day3 part 2

--- src/day03.rs
+++ src/day03.rs
@@ -3,18 +3,28 @@ mod tests {
     use super::*;
 
     #[test]
-    fn example() {
+    fn test_power_consumption() {
         let input = aoc::vector_from_file::<String>("input/day03/example").unwrap();
-        let (gamma, epsilon) = power_consumption(input);
+        let (gamma, epsilon) = power_consumption(&input);
         assert_eq!(gamma, 22);
         assert_eq!(epsilon, 9);
         assert_eq!(gamma * epsilon, 198);
     }
+
+    #[test]
+    fn test_life_support_rating() {
+        let input = aoc::vector_from_file::<String>("input/day03/example").unwrap();
+        let oxygen = life_support_rating(&input, '1');
+        let co2 = life_support_rating(&input, '0');
+        assert_eq!(oxygen, 23);
+        assert_eq!(co2, 10);
+        assert_eq!(oxygen * co2, 230);
+    }
 }
 
-fn power_consumption(input: Vec<String>) -> (u32, u32) {
+fn power_consumption(input: &Vec<String>) -> (u32, u32) {
     let mut bits: Vec<u32> = vec![0; input[0].len()];
-    for line in &input {
+    for line in input {
         for (i, char) in line.chars().enumerate() {
             if char == '1' {
                 bits[i] += 1;
@@ -39,9 +49,52 @@ fn power_consumption(input: Vec<String>) -> (u32, u32) {
     (gamma, epsilon)
 }
 
+fn life_support_rating(input: &Vec<String>, mode: char) -> u32 {
+    let mut input = input.clone();
+    let binlen = input[0].len();
+    for n in 0..binlen {
+        // for bit n:
+        let (ones, zeros): (Vec<Option<String>>, Vec<Option<String>>) = input
+            .iter()
+            .map(|x| {
+                if x.chars().nth(n) == Some(mode) {
+                    (Some(x.clone()), None)
+                } else {
+                    (None, Some(x.clone()))
+                }
+            })
+            .unzip();
+        let ones: Vec<_> = ones.into_iter().filter_map(|x| x).collect();
+        let zeros: Vec<_> = zeros.into_iter().filter_map(|x| x).collect();
+
+        if mode == '1' {
+            if ones.len() >= zeros.len() {
+                input = ones;
+            } else {
+                input = zeros;
+            }
+        } else {
+            if zeros.len() >= ones.len() {
+                input = ones;
+            } else {
+                input = zeros;
+            }
+        }
+        if input.len() == 1 {
+            break;
+        }
+    }
+    u32::from_str_radix(input.get(0).unwrap(), 2).unwrap()
+}
+
 fn main() {
     let input = aoc::vector_from_file::<String>("input/day03/input").unwrap();
-    let (gamma, epsilon) = power_consumption(input);
+    let (gamma, epsilon) = power_consumption(&input);
     println!("Gamma: {}, Epsilon: {}", gamma, epsilon);
     println!("Part 1: {}", gamma * epsilon);
+
+    let oxygen = life_support_rating(&input, '1');
+    let co2 = life_support_rating(&input, '0');
+    println!("Oxygen: {}, CO2: {}", oxygen, co2);
+    println!("Part 2: {}", oxygen * co2);
 }
